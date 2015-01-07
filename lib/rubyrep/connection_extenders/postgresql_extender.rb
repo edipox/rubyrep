dd1d3219d1aaf87bd61cb2fd5bbe7403d408a8c9
Edipo Vinicius da Silva <edipo@iporaitech.com>
2015-01-07 16:06:49 -0300

    debug error added

--- lib/rubyrep/connection_extenders/postgresql_extender.rb
+++ lib/rubyrep/connection_extenders/postgresql_extender.rb
@@ -189,6 +189,7 @@ module RR
       # Sets the schema search path as per configuration parameters
       def initialize_search_path
         schema_search_path_from_config = config[:schema_search_path] || ['public']
+        raise schema_search_path_from_config.inspect
         execute "SET search_path TO #{schema_search_path_from_config.join(',')}"
       end
 
